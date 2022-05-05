23878940719bf0397c1b23f2440f5024923d569e
immThnh <103478846+immThnh@users.noreply.github.com>
2022-05-06 01:12:43 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 int main()
 {
-    MangSoNguyen msn;
+    // khai bao
+    MangSoNguyen msn, msn1;
+    cout <<"NHAP MANG 1: ";
     cin >> msn;
-
-    return 0;
+    cout <<"NHAP MANH 2: ";
+    cin >> msn1;
+    cout << "+: " << msn + msn1 endl;
+    msn1 = msn;
+    cout <<"=: (GAN MSN1 = MSN) " << msn1 << endl;
+    
 }
